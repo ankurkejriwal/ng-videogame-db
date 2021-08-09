4d4e8ce2e930ec4907455ffa2747f3a2ea7f12f9
Ankur Kejriwal <ankurkejriwal@ankurs-mbp.lan>
2021-08-09 18:49:43 -0400

    Added getGameDetails

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http/http.service';
 
 @Component({
   selector: 'app-details',
@@ -14,11 +18,31 @@ export class DetailsComponent implements OnInit {
   //                       value = "gameRating"
 
   gameRating:number = 0;
+  gameID: string = "";
+  game! : Game;
+  routeSub: Subscription = new Subscription();
+  gameSub: Subscription = new Subscription();
 
-  constructor() { }
+  
+
+  constructor(private activatedRoute: ActivatedRoute,private httpService: HttpService) { }
 
   
   ngOnInit(): void {
+    this.routeSub = this.activatedRoute.params.subscribe((params: Params)=>{
+      this.gameID = params['id'];
+      this.getGameDetails(this.gameID);
+    });
+  }
+  getGameDetails(gameID: string):void {
+    this.gameSub = this.httpService.getGameDetails(gameID).subscribe((gameResp: Game) => {
+      this.game = gameResp;
+
+      setTimeout(() =>{
+        this.gameRating = this.game.metacritic;
+
+      },1000);
+    })
   }
 
   getColor(value: number): string{
